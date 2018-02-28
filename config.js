724fe6254373c9b25c8336a2c17fc75f476c738c
Alanna Bailey <abaileyjane@gmail.com>
2018-02-27 22:39:52 -0500

    added delete endpoint and testing

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user1:user1@ds151558.mlab.com:51558/day-by-day'
 exports.PORT = process.env.PORT || 8080;
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://user1:user1@ds151508.mlab.com:51508/day-by-day-testing'
+exports.TEST_PORT = process.env.PORT || 7070;
+
+
