27d03d6e392c8ce95e410bc51192e7e3c53b4d2f
Alanna Bailey <abaileyjane@gmail.com>
2018-03-08 06:47:58 -0500

    did auth0

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://user1:us
 exports.TEST_PORT = process.env.PORT || 7070;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'alanna';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.CLIENT_ORIGIN= process.env.CLIENT_ORIGIN || 'http://localhost:3000'
+
 
 
 
